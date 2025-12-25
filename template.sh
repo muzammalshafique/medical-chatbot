90d877af9490ca1f566ef725d2638998b6d3ff83
Muzammal Shafique <89746959+muzammalshafique@users.noreply.github.com>
2025-12-25 12:11:16 -0500

    folder structure

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkrdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .evn
+touch setup.py
+touch app.py
+touch readme.md
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Done"
